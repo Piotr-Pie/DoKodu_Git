29810e1b5b46a4b803a2c8957eab7533365e844f
Piotr <piniek266@gmail.com>
2023-12-17 13:26:05 +0100

    Zadaie 6: JEst stworzona lista kraji, zdaniem było dodać nową listę kraji.
    W przyszłości mozna przerobić, na program, gdzie ręcznie wpisujemy dane i dodajemy nowe kraje.
    
    Projekt do rozwoju!!

---
+++ Zadania_DoKodu/Modul_3/6_Modul_3__Zadanie_6.py
@@ -0,0 +1,35 @@
+Countrys = [
+    {
+    'ID': 1,
+    'Kraj': 'Polska',
+    'Stolica': 'Warszawa',
+    'Język Urzędowy': 'Polski'
+},
+
+    {
+        'ID': 2,
+        'Kraj': 'Niemcy',
+        'Stolica': 'Berlin',
+        'Język Urzędowy': 'Niemiecki'
+    },
+
+    {
+        'ID': 3,
+        'Kraj': 'USA',
+        'Stolica': 'Waszyngton',
+        'Język Urzędowy': 'Angielski'
+    },
+]
+
+
+print(Countrys)
+
+New_Country = {'ID': 4,
+                'Kraj': 'Rosja',
+                'Stolica': 'Moskwa',
+                'Język Urzędowy': 'Rosyjski'
+                }
+
+Countrys.append(New_Country)
+
+print(Countrys)

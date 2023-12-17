015c3833e6bafa7b21406936386610f7a260722c
Piotr <piniek266@gmail.com>
2023-12-17 12:33:18 +0100

    Zadaie 5: Podobnie jak w poprzednim zadaniu napisz prosgram, który stworzy słownik zawierający informacje o twoic ulubionych zwierzętach domowych. NAstępnie znajdź zwierzę, które ma najdłuższą nazwę i wyświetl jego nazwę i rasę.

---
+++ Zadania_DoKodu/Modul_3/5_modul_3__Zadanie_5.py
@@ -0,0 +1,44 @@
+#Słownik zwierząt
+animals = [
+    {
+    'ID': 1,
+    'Name': 'Luna',
+    'Rasa': 'Pies',
+    'Ilosc_nog': 4
+},
+
+    {
+    'ID': 2,
+    'Name': 'Kryska',
+    'Rasa': 'Agama Brodata',
+    'Ilosc_nog': 4
+},
+
+    {
+    'ID': 3,
+    'Name': 'Barbara',
+    'Rasa': 'Pajak',
+    'Ilosc_nog': 8
+},
+
+    {
+    'ID': 4,
+    'Name': 'Monika',
+    'Rasa': 'Zona',
+    'Ilosc_nog': 2
+},
+
+]
+
+names = []
+number = []
+for x in animals:
+    if x['Name'] not in names:
+        names.append(x['Name'])
+
+name_legs = [len(name) for name in names]
+max_leg = max(name_legs)
+
+longest_names = [name for name in names if len(name) == max_leg]
+
+print(longest_names)

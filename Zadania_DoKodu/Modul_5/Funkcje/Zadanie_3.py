13bea90d65578ce43f5159607c0c0ea8ab7cbce9
Piotr <piniek266@gmail.com>
2024-01-07 18:11:29 +0100

    # Przygotuj funkcję, która zwróci True albo False, w zależności od # tego czy liczba ta jest pierwsza.

---
+++ Zadania_DoKodu/Modul_5/Funkcje/Zadanie_3.py
@@ -0,0 +1,17 @@
+# Przygotuj funkcję, która zwróci True albo False, w zależności od
+# tego czy liczba ta jest pierwsza.
+
+def czy_pierwsza(n):
+    if n == 2:
+        return True
+    if n % 2 == 0 or n <= 1:
+        return False
+
+    pierw = int(n**0.5) + 1
+    for dzielnik in range(3, pierw, 2):
+        if n % dzielnik == 0:
+            return False
+    return True
+
+
+print(czy_pierwsza(3))
